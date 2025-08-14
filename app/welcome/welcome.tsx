870a4fb506cb40e676dcb8ce245e63f9bcf589e1
nunocpt <nuno_costa_kj@hotmail.com>
2025-08-14 23:01:14 +0100

    added breaking change

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -32,7 +32,7 @@ export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <h1>Hello world</h1>
+        <h1>wqdwqdwq</h1>
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
